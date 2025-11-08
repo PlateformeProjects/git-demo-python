76751054213d8cc56864661e5bc282266e7550f4
Mangoz404 <devendev.pro@gmail.com>
2025-11-08 07:36:50 +0100

    test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.calculator import add, subtract
 def main():
 	print( 'Going to add...' )
 	print( add(10, 5) )
+	print( add(10, 5) )
 	print( 'Going to substract...' )
 	
 	print( subtract(10, 5) )
